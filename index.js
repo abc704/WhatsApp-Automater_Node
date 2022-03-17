87b35b6011b77bdfa43b92a7b849391313ec7de8
shashank <56788944+abc704@users.noreply.github.com>
2022-03-17 12:37:04 +0530

    Update index.js
    no. change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const wbm = require("./wbm");
 
 wbm.start({ session: false, showBrowser: false }).then(async () => {
-    const phones = ['918699900057', '918446000022'];
+    const phones = ['917004346065', '919599865325'];
     const message = 'Test: automated message';
     await wbm.send(phones, message);
     await wbm.end();
